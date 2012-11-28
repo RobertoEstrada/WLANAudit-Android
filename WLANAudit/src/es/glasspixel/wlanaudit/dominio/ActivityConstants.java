50eca7259dc59a7833cca3a9c913756e76949458
Roberto Estrada <robestradac@gmail.com>
2012-11-28 01:06:19 +0100

    Dead code and library removal.

--- WLANAudit/src/es/glasspixel/wlanaudit/dominio/ActivityConstants.java
+++
@@ -1,7 +0,0 @@
-package es.glasspixel.wlanaudit.dominio;
-
-public interface ActivityConstants {
-	public static final int ACTIVITY_1 = 1001;
-	public static final int ACTIVITY_2 = 1002;
-	public static final int ACTIVITY_3 = 1003;
-}
