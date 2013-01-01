b861660331276d420853029ca2a4827b2dc76279
jesus <manzanocaminojesus@gmail.com>
2013-01-01 20:02:39 +0100

    Fix novocation invocation issue.

--- WLANAudit/src/es/glasspixel/wlanaudit/dialogs/NetworkDetailsDialogFragment.java
+++ WLANAudit/src/es/glasspixel/wlanaudit/dialogs/NetworkDetailsDialogFragment.java
@@ -243,69 +243,44 @@ public class NetworkDetailsDialogFragment extends RoboDialogFragment {
 						.where(C.eq("m_bssid", mNetworkData.BSSID)).count()
 						.getQuery()));
 
-		if (exists == 0) {
+		if (exists > 0) {
+			mStarNetworkButton.setEnabled(false);
+		}
 
-			// Setting up button callbacks
-			mCopyPasswordButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					if (mKeyList.size() == 1) {
-						copyClipboard(mNetworkDefaultPassTextView.getText()
-								.toString());
-					} else if (mKeyList.size() > 1) {
-						Intent i = new Intent(getActivity(),
-								KeyListActivity.class);
-						i.putStringArrayListExtra(KeyListActivity.KEY_LIST_KEY,
-								(ArrayList<String>) mKeyList);
-						startActivity(i);
+		mStarNetworkButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				saveNetwork(mNetworkData, mLastKnownLocation);
+				dismiss();
+			}
+		});
 
-					}
-					dismiss();
-				}
-			});
-			mStarNetworkButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					saveNetwork(mNetworkData, mLastKnownLocation);
-					dismiss();
-				}
-			});
+		mStarNetworkButton.setOnLongClickListener(new OnLongClickListener() {
 
-			mStarNetworkButton
-					.setOnLongClickListener(new OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				Toast.makeText(getActivity(),
+						getResources().getString(R.string.save_and_copy_text),
+						Toast.LENGTH_LONG).show();
+				return false;
+			}
+		});
 
-						@Override
-						public boolean onLongClick(View v) {
-							Toast.makeText(
-									getActivity(),
-									getResources().getString(
-											R.string.save_and_copy_text),
-									Toast.LENGTH_LONG).show();
-							return false;
-						}
-					});
-
-			// Calculating key
-			IKeyCalculator keyCalculator = KeyCalculatorFactory
-					.getKeyCalculator(new NetData(mNetworkData.SSID,
-							mNetworkData.BSSID));
-			if (keyCalculator != null) {
-				mKeyList = keyCalculator.getKey(new NetData(mNetworkData.SSID,
+		// Calculating key
+		IKeyCalculator keyCalculator = KeyCalculatorFactory
+				.getKeyCalculator(new NetData(mNetworkData.SSID,
 						mNetworkData.BSSID));
-				if (mKeyList != null) {
-					if (mKeyList.size() > 1) {
-						mNetworkDefaultPassTextView.setText(String
-								.valueOf(mKeyList.size())
-								+ " "
-								+ getText(R.string.number_of_keys_found));
-					} else if (mKeyList.size() == 1) {
-						mNetworkDefaultPassTextView.setText(mKeyList.get(0));
-					}
-				} else {
-					mNetworkDefaultPassTextView
-							.setText(getString(R.string.no_default_key));
-					mCopyPasswordButton.setEnabled(false);
-					mStarNetworkButton.setEnabled(false);
+		if (keyCalculator != null) {
+			mKeyList = keyCalculator.getKey(new NetData(mNetworkData.SSID,
+					mNetworkData.BSSID));
+			if (mKeyList != null) {
+				if (mKeyList.size() > 1) {
+					mNetworkDefaultPassTextView.setText(String.valueOf(mKeyList
+							.size())
+							+ " "
+							+ getText(R.string.number_of_keys_found));
+				} else if (mKeyList.size() == 1) {
+					mNetworkDefaultPassTextView.setText(mKeyList.get(0));
 				}
 			} else {
 				mNetworkDefaultPassTextView
@@ -313,11 +288,31 @@ public class NetworkDetailsDialogFragment extends RoboDialogFragment {
 				mCopyPasswordButton.setEnabled(false);
 				mStarNetworkButton.setEnabled(false);
 			}
-
 		} else {
-
+			mNetworkDefaultPassTextView
+					.setText(getString(R.string.no_default_key));
+			mCopyPasswordButton.setEnabled(false);
 			mStarNetworkButton.setEnabled(false);
 		}
+
+		// Setting up button callbacks
+		mCopyPasswordButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mKeyList.size() == 1) {
+					copyClipboard(mNetworkDefaultPassTextView.getText()
+							.toString());
+				} else if (mKeyList.size() > 1) {
+					Intent i = new Intent(getActivity(), KeyListActivity.class);
+					i.putStringArrayListExtra(KeyListActivity.KEY_LIST_KEY,
+							(ArrayList<String>) mKeyList);
+					startActivity(i);
+
+				}
+				dismiss();
+			}
+		});
+
 	}
 
 	private void setupLocationServices() {
