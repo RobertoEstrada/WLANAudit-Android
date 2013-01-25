92d074ac077708051754c57dd08e037f6fc4ec07
Roberto Estrada <robestradac@gmail.com>
2013-01-25 02:34:02 +0100

    First working GMaps 2 version.

--- WLANAudit/src/es/glasspixel/wlanaudit/activities/SlidingMapActivity.java
+++ WLANAudit/src/es/glasspixel/wlanaudit/activities/SlidingMapActivity.java
@@ -15,260 +15,335 @@
  */
 package es.glasspixel.wlanaudit.activities;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import javax.annotation.Nullable;
+
+import org.orman.mapper.Model;
 
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 import roboguice.util.RoboContext;
-
 import android.annotation.TargetApi;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
 import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.espian.showcaseview.ShowcaseView;
-import com.google.inject.Key;
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.inject.Inject;
+import com.novoda.location.provider.LastLocationFinder;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.SlidingMenu.OnOpenListener;
 
 import es.glasspixel.wlanaudit.R;
 import es.glasspixel.wlanaudit.database.entities.Network;
-import es.glasspixel.wlanaudit.fragments.MapFragment;
 import es.glasspixel.wlanaudit.fragments.SavedNetworksMenuFragment;
-import es.glasspixel.wlanaudit.fragments.MapFragment.OnMapNetworkSelected;
 import es.glasspixel.wlanaudit.fragments.SavedNetworksMenuFragment.OnSavedKeySelectedListener;
 
 public class SlidingMapActivity extends SlidingFragmentActivity implements
-		OnSavedKeySelectedListener, RoboContext, OnMapNetworkSelected,
-		ShowcaseView.OnShowcaseEventListener {
+        OnSavedKeySelectedListener, RoboContext, ShowcaseView.OnShowcaseEventListener {
+    /**
+     * Key to store and recover the map fragment in/from the saved state bundle
+     */
+    private static final String MAP_FRAGMENT_KEY = "MAP_FRAGMENT";
+    /**
+     * Constant which represents the action on the actionbar that opens the side menu
+     */
+    private static final int SHOW_MENU = 0;
+    /**
+     * GMaps V2 map fragment
+     */
+    private SupportMapFragment mMapFragment;    
+    /**
+     * GMaps V2 map controller
+     */
+    private GoogleMap mMap;
+    
+    @Inject
+    private LocationManager mLocationManager;
 
-	protected HashMap<Key<?>, Object> scopedObjects = new HashMap<Key<?>, Object>();
+    @InjectView(R.id.showcase)
+    @Nullable
+    ShowcaseView mShowcaseView;
 
-	private static final int SHOW_MENU = 0;
+    @InjectResource(R.drawable.shadow)
+    Drawable showcase_shadow;
 
-	private MapFragment mContent;
-	@InjectView(R.id.showcase)
-	@Nullable
-	ShowcaseView sv;
+    @InjectView(R.id.showcase_button)
+    @Nullable
+    Button showcase_button;
 
-	@InjectView(R.id.showcase_button)
-	@Nullable
-	Button showcase_button;
+    @InjectResource(R.string.map_layout_locations_list_title)
+    String ab_title;
 
-	@InjectResource(R.string.map_layout_locations_list_title)
-	String ab_title;
+    @InjectResource(R.string.show_keys_list)
+    String show_keys_list;
 
-	@InjectResource(R.string.show_keys_list)
-	String show_keys_list;
+    @InjectResource(R.drawable.ic_menu_account_list)
+    Drawable ic_menu_account_list;
 
-	@InjectResource(R.drawable.ic_menu_account_list)
-	Drawable ic_menu_account_list;
+    private boolean showcaseView = false;
 
-	@InjectResource(R.drawable.shadow)
-	Drawable showcase_shadow;
-
-	private boolean showcaseView = false;
-
-	@SuppressWarnings("deprecation")
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    /**
+     * {@inheritDoc}
+     */
     @Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setTitle(ab_title);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(ab_title);
+        setContentView(R.layout.responsive_content_frame);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		setContentView(R.layout.responsive_content_frame);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // UI form factor check
+        if (findViewById(R.id.menu_frame) == null) {
+            // If this is phone UI
+            showcaseView = true;
+            setBehindContentView(R.layout.menu_frame);
+            setSlidingActionBarEnabled(false);
+            getSlidingMenu().setSlidingEnabled(true);
+            getSlidingMenu().setMode(SlidingMenu.LEFT);
+            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        } else {
+            // If this is tablet UI
+            setBehindContentView(new View(this));
+        }
 
-		// check if the content frame contains the menu frame
-		if (findViewById(R.id.menu_frame) == null) {
-			showcaseView = true;
-			setBehindContentView(R.layout.menu_frame);
-			setSlidingActionBarEnabled(false);
-			getSlidingMenu().setSlidingEnabled(true);
-			getSlidingMenu().setMode(SlidingMenu.LEFT);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        // set the Above View Fragment
+        if (savedInstanceState != null)
+            mMapFragment = (SupportMapFragment) getSupportFragmentManager().getFragment(
+                    savedInstanceState, MAP_FRAGMENT_KEY);
+        if (mMapFragment == null) {
+            mMapFragment = SupportMapFragment.newInstance();
+        }
 
-		} else {
+        // Set the map fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, mMapFragment)
+                .commit();
 
-			View v = new View(this);
-			setBehindContentView(v);
-			getSlidingMenu().setSlidingEnabled(false);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-		}
+        // Set the network list fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.menu_frame, new SavedNetworksMenuFragment()).commit();
 
-		// set the Above View Fragment
-		if (savedInstanceState != null)
-			mContent = (MapFragment) getSupportFragmentManager().getFragment(
-					savedInstanceState, "mContent");
-		if (mContent == null) {
-			mContent = new MapFragment();
-		}
+        // customize the SlidingMenu
+        SlidingMenu sm = getSlidingMenu();
+        setUpSlidingMenu(sm);
+        
+        // Set up the showcase view
+        setUpShowCaseView(sm);
+        
+        // Setup the map
+        setUpMapIfNeeded();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void onResume() {
+        super.onResume();
+        // Setup the map
+        setUpMapIfNeeded();
+    }
 
-		// set the OSM map fragment
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.content_frame, mContent).commit();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.menu_map_location, menu);
+        
+        if (getSlidingMenu().isSlidingEnabled()) {
+            menu.add(0, SHOW_MENU, 1, show_keys_list);
+            menu.getItem(1).setIcon(ic_menu_account_list);
+            menu.getItem(1).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
 
-		// set the Behind View Fragment (saved keys fragment)
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.menu_frame, new SavedNetworksMenuFragment()).commit();
+        return super.onCreateOptionsMenu(menu);
+    }
 
-		// customize the SlidingMenu
-		SlidingMenu sm = getSlidingMenu();
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setShadowWidthRes(R.dimen.shadow_width);
-		sm.setShadowDrawable(showcase_shadow);
-		sm.setBehindScrollScale(0.25f);
-		sm.setFadeDegree(0.25f);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+    }
 
-		if (showcaseView) {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+                break;
+            case SHOW_MENU:
+                toggle();
+                break;
+        }
 
-			sv.setShotType(ShowcaseView.TYPE_ONE_SHOT);
+        return super.onOptionsItemSelected(item);
+    }
 
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-				Display display = getWindowManager().getDefaultDisplay();
-				Point size = new Point();
-				display.getSize(size);
-				sv.setShowcasePosition(0, size.y / 2);
-			} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2){
-				sv.setShowcasePosition(0, getWindowManager()
-						.getDefaultDisplay().getHeight() / 2);
-			}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, MAP_FRAGMENT_KEY, mMapFragment);
+    }
 
-			showcase_button.setOnClickListener(new OnClickListener() {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onSavedKeySelected(Network s) {
+        getSlidingMenu().showContent();
+        CameraPosition camPos = CameraPosition.builder()
+                .target(new LatLng(s.mLatitude, s.mLongitude))
+                .zoom(18).bearing(0.0f)
+                .tilt(45.0f)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+    }    
 
-				@Override
-				public void onClick(View v) {
-					if (sv.isShown()) {
-						sv.hide();
-						showcase_button.setOnClickListener(null);
-						sv.onClick(v);
+    private void setUpSlidingMenu(SlidingMenu sm) {
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(showcase_shadow);
+        sm.setBehindScrollScale(0.25f);
+        sm.setFadeDegree(0.25f);
+    }
+    
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void setUpShowCaseView(SlidingMenu sm) {
+        if (showcaseView) {
 
-					}
+            mShowcaseView.setShotType(ShowcaseView.TYPE_ONE_SHOT);
 
-				}
-			});
-			sm.setOnOpenListener(new OnOpenListener() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                Display display = getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
+                mShowcaseView.setShowcasePosition(0, size.y / 2);
+            } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2) {
+                mShowcaseView.setShowcasePosition(0, getWindowManager().getDefaultDisplay().getHeight() / 2);
+            }
 
-				@Override
-				public void onOpen() {
-					if (sv.isShown()) {
-						sv.hide();
-						sv.onClick(showcase_button);
-					}
+            showcase_button.setOnClickListener(new OnClickListener() {
 
-				}
-			});
-		}
+                @Override
+                public void onClick(View v) {
+                    if (mShowcaseView.isShown()) {
+                        mShowcaseView.hide();
+                        showcase_button.setOnClickListener(null);
+                        mShowcaseView.onClick(v);
 
-	}
+                    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+                }
+            });
+            sm.setOnOpenListener(new OnOpenListener() {
 
-		getSupportMenuInflater().inflate(R.menu.menu_map_location, menu);
-		if (getSlidingMenu().isSlidingEnabled()) {
-			menu.add(0, SHOW_MENU, 1, show_keys_list);
-			menu.getItem(1).setIcon(ic_menu_account_list);
-			menu.getItem(1).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+                @Override
+                public void onOpen() {
+                    if (mShowcaseView.isShown()) {
+                        mShowcaseView.hide();
+                        mShowcaseView.onClick(showcase_button);
+                    }
 
-		}
+                }
+            });
+        }
+    }
+    
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView
+     * MapView}) will show a prompt for the user to install/update the Google Play services APK on
+     * their device.
+     * <p>
+     * A user can return to this Activity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the Activity may not have been
+     * completely destroyed during this process (it is likely that it would only be stopped or
+     * paused), {@link #onCreate(Bundle)} may not be called again so we should call this method in
+     * {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = mMapFragment.getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+    
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mGMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        
+        List<Network> savedNetworks = Model.fetchAll(Network.class);
+        for (Network savedNetwork : savedNetworks) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(savedNetwork.mLatitude, savedNetwork.mLongitude))
+                    .title(savedNetwork.mSSID).snippet(savedNetwork.mBSSID));
+        }
+        
+        LastLocationFinder locator = new LastLocationFinder(mLocationManager);
+        Location loc = locator.getLastBestLocation(500 * 1000);
+        if (loc != null) {
+            CameraPosition camPos = CameraPosition.builder()
+                    .target(new LatLng(loc.getLatitude(), loc.getLongitude())).zoom(18)
+                    .bearing(0.0f).tilt(45.0f).build();
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onShowcaseViewHide(ShowcaseView showcaseView) {
+    }
 
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public void onBackPressed() {
-		finish();
-		overridePendingTransition(R.anim.slide_in_from_left,
-				R.anim.slide_out_to_right);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			overridePendingTransition(R.anim.slide_in_from_left,
-					R.anim.slide_out_to_right);
-			break;
-		case R.id.check_location_menu:
-
-			((MapFragment) getSupportFragmentManager().findFragmentById(
-					R.id.content_frame)).showLocation();
-			((MapFragment) getSupportFragmentManager().findFragmentById(
-					R.id.content_frame)).clearAllFocused();
-
-			break;
-		case SHOW_MENU:
-			toggle();
-
-			break;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
-	}
-
-	public void switchContent(final Fragment fragment) {
-		mContent = (MapFragment) fragment;
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.content_frame, fragment).commit();
-		Handler h = new Handler();
-		h.postDelayed(new Runnable() {
-			public void run() {
-				getSlidingMenu().showContent();
-			}
-		}, 50);
-	}
-
-	@Override
-	public void onSavedKeySelected(Network s) {
-
-		getSlidingMenu().showContent();
-		((MapFragment) getSupportFragmentManager().findFragmentById(
-				R.id.content_frame)).setFocused(s);
-
-	}
-
-	@Override
-	public Map<Key<?>, Object> getScopedObjectMap() {
-		return scopedObjects;
-	}
-
-	@Override
-	public void onShowcaseViewHide(ShowcaseView showcaseView) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onShowcaseViewShow(ShowcaseView showcaseView) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onMapNetworkSelected(int selected_network_index) {
-
-		((SavedNetworksMenuFragment) getSupportFragmentManager().findFragmentById(
-				R.id.menu_frame)).onMapItemSelected(selected_network_index);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onShowcaseViewShow(ShowcaseView showcaseView) {
+    }
 }
